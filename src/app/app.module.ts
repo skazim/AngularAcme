23a50333817b60722edfb7cc3c961ec34108b428
skazim <kazimmuhammad10@gmail.com>
2020-04-01 14:46:37 +0500

    Updating to featured Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ConvertToSpaces } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ProductDetailComponent } from './product-list/product-detail/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
-import { ProductDetailGuard } from './product-list/product-detail.guard';
+import { ProductModule } from './product-list/product.module';
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent, ConvertToSpaces, StarComponent, ProductDetailComponent, WelcomeComponent
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule,
+    BrowserModule, HttpClientModule,
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id',canActivate : [ProductDetailGuard], component: ProductDetailComponent },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ])
+    ]),
+    ProductModule
   ],
   bootstrap: [AppComponent]
 })
